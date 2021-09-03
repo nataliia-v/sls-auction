13c011d4bb2b08d16f0208a9a73369771ad32aed
NataliVerbenska <nataliia.verbenska@authenticgaming.com>
2021-09-04 01:16:18 +0300

    added middy middleware, errors handler

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,11 +1,19 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from 'http-errors';
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
 
+  /* httpJsonBodyParser will automatically pass our string defined event body, so we don't have to do that every time with just result in cleaner code.
   const { title } = JSON.parse( event.body )
+  */
+  const { title } = event.body
   const now = new Date();
 
   const auction = {
@@ -15,10 +23,15 @@ async function createAuction(event, context) {
     createdAt: now.toISOString(),
   }
 
-  await dynamoDB.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction,
-  }).promise();
+  try {
+    await dynamoDB.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }).promise();
+  } catch ( error ) {
+    console.error( error )
+    throw new createError.InternalServerError(error)
+  }
 
   return {
     statusCode: 201,
@@ -26,5 +39,13 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+export const handler = middy( createAuction )
+  .use( httpJsonBodyParser() )
+  .use( httpEventNormalizer() )
+  .use( httpErrorHandler() )
 
+/* httpEventNormalizer will automatically adjust the API gateway event objects to prevent us from accidentally having a nonexisting object
+when trying to access path parameters or query parameters and they're not provided just will save us from room for error.
+*/
+
+/* httpErrorHandler help make handling error process smooth, easy and clean */
