8cd467265f4ef8c334137331f5d71cec7c252849
NataliVerbenska <nataliia.verbenska@authenticgaming.com>
2021-09-27 21:27:42 +0300

    uploading picture to S3

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,5 +1,15 @@
+import { getAuctionById } from "./getAuction";
+import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
 
-const uploadAuctionPicture = async ( event ) => {
+export const uploadAuctionPicture = async ( event ) => {
+
+  const { id } = event.pathParameters;
+  const auction = await getAuctionById(id);
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+  const buffer = Buffer.from(base64, 'base64');
+
+  const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
+  console.log(uploadToS3Result);
 
   return {
     statusCode: 200,
